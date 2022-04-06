ffca468c54939d12b19ea75dd62413af27957980
ccbagel <hasan.abdi.ahmed97@gmail.com>
2022-04-06 02:42:38 -0600

    navbar completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { HiMenuAlt4 } from 'react-icons/hi'
 import { AiOutlineClose } from 'react-icons/ai'
 
@@ -11,6 +12,9 @@ const NavbarItem = ({ title, classProps }) => {
 }
 
 const Navbar = () => {
+    // navbar state 
+    const [toggled, setToggled] = useState(false);
+
     return (
         <nav className="w-full flex justify-between md:justify-center items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -25,6 +29,27 @@ const Navbar = () => {
                 duration-200 ease-in-out"
                 >Login</li>
             </ul>
+            <div className="flex relative">
+                {toggled ? (
+                    <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggled(false)}  />
+                ): (
+                    <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggled(true)}  />
+                )}
+                {toggled && (
+                    <ul
+                    className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
+                    "
+                    >
+                        <li className="text-xl w-full my-2">
+                            <AiOutlineClose onClick={() => setToggled(false)} />
+                        </li>
+                        {["Market", "Exchange", "Docs", "Wallet"].map((item, index) => (
+                            <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
+                        ))}
+                    </ul>
+                )}
+            </div>
         </nav>
     )
 }
